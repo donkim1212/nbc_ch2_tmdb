cf4e70d34a12064e843e183a033bccde4e94e493
Dongkyun Kim <donkim0122@gmail.com>
2024-04-26 14:43:30 +0900

    Create: index.js with scripts removed from index.html Refactor: index.html now loads index.js instead of script inside tags

---
+++ js/index.js
@@ -0,0 +1,25 @@
+import { searchMoviesByTitle, clearCachedList } from "./search-movie.js";
+import { setCardContainer, getCardContainer } from "./card.js";
+
+setInterval(clearCachedList, 60000);
+setCardContainer(document.getElementById("card-container-01"));
+searchMoviesByTitle(""); // load all movies list
+
+const searchButton = document.getElementById("search-btn-01");
+const searchBar = document.getElementById("search-bar-01");
+searchBar.focus();
+
+searchBar.addEventListener("keypress", function () {
+    if (window.event.keyCode == 13) {
+        search();
+    }
+});
+
+searchButton.addEventListener("click", function () {
+    search();
+});
+
+function search() {
+    let searched = searchBar.value;
+    searchMoviesByTitle(searched);
+}
