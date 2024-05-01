1934e770dc42947d3268afa8fa6385ed3aaa41e7
jaemin <wjdwoals9292@gmail.com>
2024-05-01 21:09:12 +0900

    [FEATURE][TMDB API] 필요한 API정보를 추가하기

--- js/fetch-movies-db.js
+++ js/fetch-movies-db.js
@@ -1,7 +1,9 @@
-// let isLocal = true; // change this value to false to load data from TMDB
+// const isLocal = true; // change this value to false to load data from TMDB
 
-const URL = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-const LOCAL_FILE = './temp.json';
+const URL = 'https://api.themoviedb.org/3/movie/';
+const TOP_RATED_LOCAL_FILE = './temp.json';
+const DETAIL_LOCAL_FILE = './detailTemp.json';
+const CREDITS_LOCAL_FILE = './creaditTemp.json';
 
 const options = {
     method: 'GET',
@@ -14,16 +16,67 @@ const options = {
 /**
  * 
  * @param {boolean} isLocal true uses local, false uses TMDB list
- * @returns list of top rated movies from retrieved data.
+ * @returns {string} - Returns the key for session storage
  */
 const fetchMoviesList = async (isLocal) => {
     try{
-        let data = await (await fetch(isLocal? LOCAL_FILE : URL, options)).json();
-        // console.log(data);
-        return data["results"];
+        const moviesList = sessionStorage.getItem('moviesList');
+
+        if(moviesList){
+            return JSON.parse(moviesList);
+        }else{
+            let data = await (await fetch(isLocal? TOP_RATED_LOCAL_FILE : `${URL}top_rated?language=en-US&page=1`, options)).json();
+            sessionStorage.setItem('moviesList', JSON.stringify(data["results"]));
+            return data["results"];
+        }
     } catch (err) {
         console.log(err);
     }
 }
 
-export { fetchMoviesList };
+/**
+ * Fetches the movie list using the provided movie ID.
+ * 
+ * @param {boolean} isLocal true uses local, false uses TMDB list
+ * @param {number} movieId - The ID of the movie to fetch the list for.
+ * @returns {Array} - The fetched movie detail list.
+ */
+const fetchMovieDetail = async (isLocal, movieId) => {
+    try{
+        const movieDetail = sessionStorage.getItem(`${movieId}Detail`);
+
+        if(movieDetail){
+            return JSON.parse(movieDetail);
+        }else{
+            let data = await (await fetch(isLocal? DETAIL_LOCAL_FILE : `${URL}${movieId}?language=en-US&page=1`, options)).json();
+            sessionStorage.setItem(`${movieId}Detail`, JSON.stringify(data["results"]));
+            return data["results"];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+/**
+ * Fetches the movie list using the provided movie ID.
+ * 
+ * @param {boolean} isLocal true uses local, false uses TMDB list
+ * @param {number} movieId - The ID of the movie to fetch the list for.
+ * @returns {Array} - The fetched movie credits list.
+ */
+const fetchMovieCredits = async (isLocal, movieId) => {
+    try{
+        const movieCredits = sessionStorage.getItem(`${movieId}Credit`);
+        if(movieCredits){
+            return JSON.parse(movieCredits);
+        }else{
+            let data = await (await fetch(isLocal? CREDITS_LOCAL_FILE : `${URL}${movieId}/credits?language=en-US`, options)).json();
+            sessionStorage.setItem(`${movieId}Credits`, JSON.stringify(data["results"]));
+            return data["results"];
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+export { fetchMoviesList, fetchMovieDetail, fetchMovieCredits };
--- js/search-movie.js
+++ js/search-movie.js
@@ -1,31 +1,57 @@
 // console.log("search-movie.js loaded");
 import { addCard, emptyCards } from "./card.js";
-import { fetchMoviesList } from "./fetch-movies-db.js";
+import { fetchMovieCredits, fetchMovieDetail, fetchMoviesList } from "./fetch-movies-db.js";
+
+const IMG_URL = "https://image.tmdb.org/t/p/w300";
 
 const searchMoviesByTitle = async (title) => {
     title = title.toLowerCase();
-    await setCachedMoviesList(false);
+    // await setCachedMoviesList(false);
 
-    let filteredList = JSON.parse(sessionStorage.getItem("cachedList"))?.filter(function (data) {
+    let moviesList = await fetchMoviesList();
+    // movieList 를 먼저 
+    let filteredList = moviesList?.filter(function (data) {
         return data["title"].toLowerCase().includes(title);
     });
-    if (!filteredList) return;
-
+    if (!filteredList || filteredList.length === 0) {
+        return;
+    }
     emptyCards();
 
     filteredList.forEach(data => {
-        let imageURL = "https://image.tmdb.org/t/p/w300" + data["poster_path"];
+        let imageURL = IMG_URL + data["poster_path"];
         addCard(imageURL, data["title"], data["overview"], data["vote_average"], data["id"]);
     });
 }
 
-const setCachedMoviesList = async (isLocal) => {
-    if (!window.sessionStorage.getItem("cachedList")) {
-        window.sessionStorage.setItem("cachedList", JSON.stringify(await fetchMoviesList(isLocal)));
-        // console.log("cached new list from " + (isLocal ? "local json." : "TMDB."));
-        return;
-    }
-    // console.log("Loading cached list");
+const searchMoviesDetailByMovieId = async (movieId) => {
+
+    let detailMovie = await fetchMovieDetail(movieId);
+    
+    return detailMovie;
+
+}
+
+const searchMoviesCreditsByMovieId = async (movieId) => {
+
+    let creditsMovie = await(setCachedMovieCredits(movieId));
+
+
+    return creditsMovie
+}
+
+
+const setCachedMovieCredits = async (movieId) =>{
+    return window.sessionStorage.getItem(await(fetchMovieCredits(movieId)));
+    // console.log("Save success:SeesionStorege Credits")
+}
+const setCachedMovieDetail = async (movieId) =>{
+    return window.sessionStorage.getItem(await(fetchMovieDetail(movieId)));
+    // console.log("Save success:SeesionStorege Detail")
+}
+const setCachedMoviesList = async () => {
+    return window.sessionStorage.getItem(await(fetchMoviesList()));
+    // console.log("Save success:SeesionStorege Top Rate ")
 }
 
 const clearCachedList = () => {
@@ -33,4 +59,5 @@ const clearCachedList = () => {
     // console.log("cleared cachedList");
 }
 
-export { searchMoviesByTitle, setCachedMoviesList, clearCachedList };
+export { searchMoviesByTitle, setCachedMoviesList, clearCachedList
+, setCachedMovieDetail, searchMoviesCreditsByMovieId, searchMoviesDetailByMovieId};
