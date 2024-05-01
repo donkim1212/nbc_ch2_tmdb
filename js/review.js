5924d2b6c1b1c13b2f38c5c9502b84067201162e
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-05-01 15:51:03 +0900

    기본 함수 완성

--- js/review.js
+++ js/review.js
@@ -1,16 +1,37 @@
-const saveReview = function (obj) {
-  const value = JSON.stringify(obj);
-  localStorage.setItem(obj.name, value);
+window.onload = function () {
+  //test
+  localStorage.clear();
+
+  let obj = { id: "255", name: "수경", content: "내용", pw: "1234" };
+  saveReview(obj);
+  let obj2 = { id: "255", name: "상우", content: "내용", pw: "4321" };
+  saveReview(obj2);
+
+  loadReview(255);
 };
 
-const loadReview = function () {
-  let reviews = Object.keys(window.localStorage);
-  reviews.forEach((review) => {
-    review;
+const saveReview = function (obj) {
+  // Format change
+  const value = JSON.stringify(obj);
+  // Save Data (Key = Random)
+  localStorage.setItem(Math.random(), value);
+};
+
+const loadReview = function (id) {
+  // Data filtering
+  const reviews = Object.keys(window.localStorage).filter((rv) => {
+    const rvData = JSON.parse(localStorage.getItem(rv));
+    return rvData.id == id;
   });
 
+  //test
+  const reviewBox = document.querySelector("#test");
   console.log(reviews);
+  reviews.forEach((rv) => {
+    const rvData = JSON.parse(localStorage.getItem(rv));
+    const item = document.createElement("h1");
+    item.innerHTML = `${rvData.name} / ${rvData.content} / ${rvData.pw}`;
+    reviewBox.appendChild(item);
+  });
+  //end test
 };
-
-obj = { name: "수연", content: "내용", pw: "1234" };
-saveReview(obj);
