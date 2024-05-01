17df072e3d6b160c2a38eadfd0e1b40ce6a29cb5
wjdrbsgkrry <wjdrbsgkrry@naver.com>
2024-05-01 17:49:11 +0900

    비속어 필터링 테스트

--- js/review.js
+++ js/review.js
@@ -1,9 +1,11 @@
+const filterChecker = ["임의"];
+
 window.onload = function () {
   // Initialization for testing
   localStorage.clear();
 
   // Test data input
-  let obj = { id: "255", name: "수경", content: "내용", pw: "1234" };
+  let obj = { id: "255", name: "임의", content: "내용", pw: "1234" };
   saveReview(obj);
   let obj2 = { id: "255", name: "상우", content: "내용", pw: "4321" };
   saveReview(obj2);
@@ -14,6 +16,50 @@ window.onload = function () {
 
 //Save Methed
 const saveReview = function (obj) {
+  //Validation check
+
+  // Name
+  const check_name = obj.name.replace(/ /g, "");
+  if (check_name.length > 3) {
+    alert("닉네임 길이는 최대 3글자를 넘을 수 없습니다.");
+    return;
+  } else if (check_name.length == 0) {
+    alert("닉네임을 다시 입력해주세요!");
+    return;
+  }
+
+  // Pw
+  const check_pw = obj.pw.replace(/ /g, "");
+  if (check_pw.length > 4) {
+    alert("비밀번호의 길이는 4개를 넘을 수 없습니다!");
+    return;
+  } else if (check_pw.length == 0) {
+    alert("비밀번호를 다시 입력해주세요!");
+    return;
+  }
+
+  // Content
+  const check_content = obj.content.replace(/ /g, "");
+  if (check_content.length > 50) {
+    alert("리뷰 내용은 50자를 넘을 수 없습니다!");
+    return;
+  } else if (check_content.length == 0) {
+    alert("비밀번호를 다시 입력해주세요!");
+    return;
+  }
+
+  // Bad language
+  for (let text of filterChecker) {
+    if (
+      check_name.includes(text) ||
+      check_pw.includes(text) ||
+      check_content.includes(text)
+    ) {
+      alert("작성 내용에는 비속어를 사용하실 수 없습니다.");
+      return;
+    }
+  }
+
   // Key Create & Apply
   obj.key = Math.random();
   // Format change
